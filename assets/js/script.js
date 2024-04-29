af4a779ce95e8444982f1429515b5b34f9b38fc5
Piva84 <pierre_wangborg@hotmail.com>
2024-04-29 08:42:00 +0000

    added the questions and the answers arranged in lists to easy acces them later

---
+++ assets/js/script.js
@@ -0,0 +1,416 @@
+const questions = {
+    0: {
+      question: "Which mountain range stretches across several countries in North Africa, including Morocco and Algeria?",
+      alternatives: {
+        a: "Kilimanjaro Range",
+        b: "Drakensberg Mountains",
+        c: "Atlas Mountains",
+        d: "Rwenzori Mountains"
+      }
+    },
+    1: {
+      question: "What is the longest river in Asia, known as the 'Father of Rivers'?",
+      alternatives: {
+        a: "Yangtze River",
+        b: "Mekong River",
+        c: "Ganges River",
+        d: "Yellow River"
+      }
+    },
+    2: {
+      question: "Which U.S. state is often referred to as the 'Lone Star State'?",
+      alternatives: {
+        a: "California",
+        b: "Texas",
+        c: "Florida",
+        d: "New York"
+      }
+    },
+    3: {
+      question: "What is the capital city of New Zealand?",
+      alternatives: {
+        a: "Sydney",
+        b: "Auckland",
+        c: "Wellington",
+        d: "Melbourne"
+      }
+    },
+    4: {
+      question: "Mount Kilimanjaro, Africa's highest peak, is located in which country?",
+      alternatives: {
+        a: "Kenya",
+        b: "Tanzania",
+        c: "Uganda",
+        d: "Ethiopia"
+      }
+    },
+    5: {
+      question: "The Yangtze River is the longest river in Asia and the third-longest in the world. It flows through which country?",
+      alternatives: {
+        a: "India",
+        b: "China",
+        c: "Vietnam",
+        d: "Thailand"
+      }
+    },
+    6: {
+      question: "Which U.S. state is known as the 'Empire State'?",
+      alternatives: {
+        a: "California",
+        b: "Texas",
+        c: "Florida",
+        d: "New York"
+      }
+    },
+    7: {
+      question: "What is the capital city of Fiji?",
+      alternatives: {
+        a: "Suva",
+        b: "Apia",
+        c: "Port Moresby",
+        d: "Nuku'alofa"
+      }
+    },
+    8: {
+      question: "The Rwenzori Mountains, also known as the 'Mountains of the Moon,' are located on the border of Uganda and which other country?",
+      alternatives: {
+        a: "Kenya",
+        b: "Tanzania",
+        c: "Democratic Republic of the Congo",
+        d: "Rwanda"
+      }
+    },
+    9: {
+      question: "Which river, known as the Brahmaputra in India, flows through Tibet, India, and Bangladesh before emptying into the Bay of Bengal?",
+      alternatives: {
+        a: "Indus River",
+        b: "Irrawaddy River",
+        c: "Brahmaputra River",
+        d: "Salween River"
+      }
+    },
+    10: {
+      question: "Which U.S. state is often referred to as the 'Granite State'?",
+      alternatives: {
+        a: "Vermont",
+        b: "New Hampshire",
+        c: "Maine",
+        d: "Rhode Island"
+      }
+    },
+    11: {
+      question: "What is the capital city of Papua New Guinea?",
+      alternatives: {
+        a: "Port Moresby",
+        b: "Honiara",
+        c: "Suva",
+        d: "Nuku'alofa"
+      }
+    },
+    12: {
+      question: "The Nile River, the longest river in the world, flows through which continent?",
+      alternatives: {
+        a: "South America",
+        b: "Africa",
+        c: "Europe",
+        d: "Asia"
+      }
+    },
+    13: {
+      question: "Which Asian country is known as the 'Land of the Rising Sun'?",
+      alternatives: {
+        a: "China",
+        b: "Japan",
+        c: "India",
+        d: "South Korea"
+      }
+    },
+    14: {
+      question: "Which U.S. state is often called the 'Golden State'?",
+      alternatives: {
+        a: "California",
+        b: "Texas",
+        c: "Florida",
+        d: "New York"
+      }
+    },
+    15: {
+      question: "What is the capital city of Australia?",
+      alternatives: {
+        a: "Sydney",
+        b: "Melbourne",
+        c: "Canberra",
+        d: "Brisbane"
+      }
+    },
+    16: {
+      question: "Victoria Falls, one of the largest waterfalls in the world, is located on the border of Zambia and which other country?",
+      alternatives: {
+        a: "Zimbabwe",
+        b: "Tanzania",
+        c: "Angola",
+        d: "Mozambique"
+      }
+    },
+    17: {
+      question: "Mount Everest, the highest peak in the world, is located in which mountain range?",
+      alternatives: {
+        a: "Himalayas",
+        b: "Andes",
+        c: "Rockies",
+        d: "Alps"
+      }
+    },
+    18: {
+      question: "Which U.S. state is known as the 'Sunshine State'?",
+      alternatives: {
+        a: "California",
+        b: "Florida",
+        c: "Texas",
+        d: "Hawaii"
+      }
+    },
+    19: {
+      question: "What is the capital city of New Caledonia?",
+      alternatives: {
+        a: "Nouméa",
+        b: "Papeete",
+        c: "Port Vila",
+        d: "Majuro"
+      }
+    },
+    20: {
+      question: "Lake Victoria, the largest lake in Africa, is bordered by which three countries?",
+      alternatives: {
+        a: "Uganda, Tanzania, and Kenya",
+        b: "Kenya, Ethiopia, and Somalia",
+        c: "Tanzania, Rwanda, and Burundi",
+        d: "Uganda, Sudan, and South Sudan"
+      }
+    },
+    21: {
+      question: "Which river, known as the 'Yellow River,' is often referred to as the cradle of Chinese civilization?",
+      alternatives: {
+        a: "Yangtze River",
+        b: "Mekong River",
+        c: "Yellow River",
+        d: "Pearl River"
+      }
+    },
+    22: {
+      question: "Which U.S. state is known as the 'Land of Enchantment'?",
+      alternatives: {
+        a: "Arizona",
+        b: "New Mexico",
+        c: "Colorado",
+        d: "Utah"
+      }
+    },
+    23: {
+      question: "What is the capital city of Vanuatu?",
+      alternatives: {
+        a: "Honiara",
+        b: "Port Moresby",
+        c: "Suva",
+        d: "Port Vila"
+      }
+    },
+    24: {
+      question: "The Sahara Desert, the largest hot desert in the world, is located on which continent?",
+      alternatives: {
+        a: "Asia",
+        b: "Africa",
+        c: "South America",
+        d: "Australia"
+      }
+    },
+    25: {
+      question: "Which country is located at the crossroads of Europe and Asia?",
+      alternatives: {
+        a: "China",
+        b: "India",
+        c: "Russia",
+        d: "Turkey"
+      }
+    },
+    26: {
+      question: "Which U.S. state is known as the 'Peach State'?",
+      alternatives: {
+        a: "Georgia",
+        b: "Florida",
+        c: "Louisiana",
+        d: "Alabama"
+      }
+    },
+    27: {
+      question: "What is the capital city of Fiji?",
+      alternatives: {
+        a: "Suva",
+        b: "Apia",
+        c: "Port Moresby",
+        d: "Nuku'alofa"
+      }
+    },
+    28: {
+      question: "What is the highest mountain in Africa?",
+      alternatives: {
+        a: "Mount Kenya",
+        b: "Mount Kilimanjaro",
+        c: "Mount Elgon",
+        d: "Mount Meru"
+      }
+    },
+    29: {
+      question: "Which river is the longest river entirely within Russia?",
+      alternatives: {
+        a: "Amur River",
+        b: "Ob River",
+        c: "Lena River",
+        d: "Yenisei River"
+      }
+    },
+    30: {
+      question: "What is the capital city of France?",
+      alternatives: {
+        a: "Madrid",
+        b: "Rome",
+        c: "Paris",
+        d: "Berlin"
+      }
+    },
+    31: {
+      question: "Which European country is known for its fjords, stunning landscapes, and midnight sun during summer?",
+      alternatives: {
+        a: "Norway",
+        b: "Sweden",
+        c: "Iceland",
+        d: "Finland"
+      }
+    },
+    32: {
+      question: "The Danube River, the second-longest river in Europe, flows through which capital city?",
+      alternatives: {
+        a: "Budapest",
+        b: "Vienna",
+        c: "Prague",
+        d: "Warsaw"
+      }
+    },
+    33: {
+      question: "Which South American country is home to the world's largest rainforest, the Amazon Rainforest?",
+      alternatives: {
+        a: "Brazil",
+        b: "Argentina",
+        c: "Colombia",
+        d: "Peru"
+      }
+    },
+    34: {
+      question: "What is the highest waterfall in the world, located in Venezuela?",
+      alternatives: {
+        a: "Iguazu Falls",
+        b: "Angel Falls",
+        c: "Victoria Falls",
+        d: "Niagara Falls"
+      }
+    },
+    35: {
+      question: "The Andes, the longest continental mountain range in the world, run through several South American countries. Which of these countries does not have the Andes within its borders?",
+      alternatives: {
+        a: "Argentina",
+        b: "Colombia",
+        c: "Brazil",
+        d: "Chile"
+      }
+    },
+    36: {
+      question: "What is the capital city of Italy?",
+      alternatives: {
+        a: "Paris",
+        b: "Rome",
+        c: "Madrid",
+        d: "Athens"
+      }
+    },
+    37: {
+      question: "The Black Forest, known for its dense, dark woods, is located in which European country?",
+      alternatives: {
+        a: "Switzerland",
+        b: "Germany",
+        c: "Austria",
+        d: "France"
+      }
+    },
+    38: {
+      question: "Which South American country is known for the iconic Machu Picchu ruins?",
+      alternatives: {
+        a: "Peru",
+        b: "Chile",
+        c: "Ecuador",
+        d: "Bolivia"
+      }
+    },
+    39: {
+      question: "What is the capital city of Argentina?",
+      alternatives: {
+        a: "Santiago",
+        b: "Rio de Janeiro",
+        c: "Buenos Aires",
+        d: "Lima"
+      }
+    },
+    40: {
+      question: "The Matterhorn, one of the highest peaks in the Alps, straddles the border between Switzerland and which other country?",
+      alternatives: {
+        a: "Italy",
+        b: "Austria",
+        c: "France",
+        d: "Germany"
+      }
+    }
+  };
+  
+  const correctAnswers = {
+    0: "c",
+    1: "c",
+    2: "b",
+    3: "c",
+    4: "b",
+    5: "b",
+    6: "d",
+    7: "a",
+    8: "c",
+    9: "c",
+    10: "b",
+    11: "d",
+    12: "b",
+    13: "b",
+    14: "a",
+    15: "c",
+    16: "a",
+    17: "a",
+    18: "b",
+    19: "a",
+    20: "a",
+    21: "c",
+    22: "b",
+    23: "d",
+    24: "b",
+    25: "d",
+    26: "a",
+    27: "a",
+    28: "b",
+    29: "c",
+    30: "c",
+    31: "a",
+    32: "b",
+    33: "a",
+    34: "b",
+    35: "c",
+    36: "b",
+    37: "b",
+    38: "a",
+    39: "c",
+    40: "a"
+  };
+  
